dd61090e328682e24120714ef2be88349ff85236
tjchenzui <tj_529273815@163.com>
2017-09-25 10:42:13 +0800

    m删除runtime 和 upload 下面的 .gitgnore 文件

--- data/runtime/cache/30/6cdd0f309661b0e0e78338d8e94de3.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:4:{s:9:"site_name";s:18:"学生测试系统";s:14:"site_seo_title";s:18:"学生测试系统";s:17:"site_seo_keywords";s:18:"学生测试系统";s:20:"site_seo_description";s:51:"为学生打造的一款在线学习交流的平台";}
--- data/runtime/cache/3c/017ccaa47cbb4c3401209eedada36a.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{i:0;a:2:{s:4:"name";s:16:"MainContributors";s:9:"is_system";i:1;}i:1;a:2:{s:4:"name";s:6:"CmfHub";s:9:"is_system";i:1;}i:2;a:2:{s:4:"name";s:12:"Contributors";s:9:"is_system";i:1;}}
